05ebbcb9703b6f153f23eaa7eba0e0fea784ab6d
Karakannn <eminkarakan0@gmail.com>
2025-04-21 16:23:44 +0300

    updated middleware for localeDetection

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,8 +4,8 @@ import { locales } from "./i18n";
 export default createMiddleware({
   locales: locales,
   defaultLocale: "en",
-  localePrefix: "as-needed",
-  localeDetection: true,
+  localePrefix: "always", 
+  localeDetection: false, 
 });
 
 export const config = {
